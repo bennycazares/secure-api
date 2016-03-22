97fdf3831bb0565023dbc2b665ddd3cdabdc6671
Benito Cazares IV <Benny@benitos-mbp.attlocal.net>
2016-03-22 00:01:30 -0500

    add crud for posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     get "posts" => 'posts#index'
     get "posts/:id" => 'posts#show'
-
+    post "orders" => 'orders#create'
+    patch "orders/:id" => 'orders#update'
+    delete "orders/:id" => 'orders#delete'
     post "registrations" => 'registrations#create'
 
   end
